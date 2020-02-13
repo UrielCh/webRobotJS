b6d9cddf2e0c51b17ad357798f27b76e33500d6d
urielCh <uriel.chemouni@gmail.com>
2020-02-13 21:55:00 +0200

    fix kill previous service

--- zService.js
+++ zService.js
@@ -1,21 +1,22 @@
 // @ts-check
+/* eslint indent: ["error", 4, { "ignoredNodes": ["ConditionalExpression"] }] */
 
 const rp = require('request-promise');
 const delay = require('delay');
 
-/* eslint indent: ["error", 4, { "ignoredNodes": ["ConditionalExpression"] }] */
-/**
- * only fun service on port 9000
- */
-// var environementP =
-
-let servicePort = 9000;
-
-if (servicePort > 0) {
-  console.log('Try to kill old service on port', servicePort)
-  rp(`http://127.0.0.1:${servicePort}/kill`)
-    .then(undefined, () => Promise.resolve()).then(delay(400)).then(() => {
-      console.log('Starting now')
-      require('./web').listen(servicePort)
-    })
+async function killRun(servicePort) {
+  console.log('Try to kill old service on port', servicePort);
+  let pass = 0;
+  try {
+    while (pass < 20) {
+      await rp(`http://127.0.0.1:${servicePort}/kill`);
+      await delay(1000);
+    }
+  } catch (e) {
+    console.log('Starting now');
+    require('./web').listen(servicePort);
+    return;
+  }
+  console.error(`${servicePort} is bussy`);
 }
+killRun(9000);
